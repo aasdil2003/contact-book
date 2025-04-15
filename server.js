7d12f213a832f423f995ea928b8e7f0f89a808ee
aadil verma <122432210+aasdil2003@users.noreply.github.com>
2025-04-15 22:07:10 +0530

    server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const contactRoutes = require('./routes/contactRoutes');
+
+dotenv.config(); 
+
+const app = express();
+app.use(express.json());
+
+app.use('/api', contactRoutes);
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+    console.log('MongoDB Connected');
+})
+.catch((err) => {
+    console.error('MongoDB connection error:', err);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
+
