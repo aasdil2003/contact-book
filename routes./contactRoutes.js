cdff52aae5b664fcf761fbf0b4244e0943b4af5e
aadil verma <122432210+aasdil2003@users.noreply.github.com>
2025-04-17 17:47:58 +0530

    contactRoutes.js

--- routes./contactRoutes.js
+++ routes./contactRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const express = require('express');
 const router = express.Router();
-const { addContact } = require('../controllers/contactcontroller');
+const { addContact, getAllContacts } = require('../controllers/contactcontroller');
 
 router.post('/contacts', addContact);
 
+router.get('/contacts', getAllContacts);
 
 module.exports = router;
